3350c6a352102b1d82770c008b436e9ba0d76325
Manish Giri <manish.giri.me@gmail.com>
2015-11-17 23:04:25 -0500

    selecting multiple elements and setting text content

---
+++ Chap-02/js/string_variable.js
@@ -0,0 +1,8 @@
+var username = 'Molly';
+var message = "See our upcoming range";
+
+var el1 = document.getElementById('name');
+el1.textContent = username;
+
+var el2 = document.getElementById('note');
+el2.textContent = message;

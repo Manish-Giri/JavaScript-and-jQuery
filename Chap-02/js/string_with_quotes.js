5a76660bbfa22ca0b1eb24ac11458eee0d004f11
Manish Giri <manish.giri.me@gmail.com>
2015-11-17 23:05:05 -0500

    using .innerHTML to show special markup

---
+++ Chap-02/js/string_with_quotes.js
@@ -0,0 +1,8 @@
+var title = "Molly's special offers";
+var message = '<a href=\"sale.html\">25% off!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;

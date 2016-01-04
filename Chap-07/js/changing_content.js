2d544388cedbeada24e061372a54fa31eb84bcb7
Manish Giri <manish.giri.me@gmail.com>
2016-01-04 16:14:20 -0500

    changing content with .text()

---
+++ Chap-07/js/changing_content.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("li:contains('pine')").text("almonds");
+    $("li.hot").html(function () {
+        return "<em>" + $(this).text() + "</em>";
+    });
+
+    $("li#one").remove();
+});

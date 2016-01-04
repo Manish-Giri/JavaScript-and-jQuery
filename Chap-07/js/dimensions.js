336c56d2bf884e756d90be71fe99d6c90755a47f
Manish Giri <manish.giri.me@gmail.com>
2016-01-04 16:15:41 -0500

    changing dimensions with .width()

---
+++ Chap-07/js/dimensions.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var listHeight = $("#page").height();
+    $("ul").append('<p>Height: ' + listHeight + 'px</p>');
+
+    $('li').width("50%");
+    $("li#one").width(125);
+    $("li#two").width("75%");
+
+
+});
